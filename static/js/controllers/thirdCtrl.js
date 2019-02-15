94ed28774bd62aad5b828120e447099dd93157d2
Zuza <zuzannaprochownik7@gmail.com>
2019-02-15 20:17:44 +0200

    s

--- static/js/controllers/thirdCtrl.js
+++ static/js/controllers/thirdCtrl.js
@@ -16,83 +16,26 @@ angular.module('myApp.controllers').controller('thirdCtrl',
             //dane z https://jsonplaceholder.typicode.com/photos
 
 
-            $scope.pictures = [
-                {
-                    "albumId": 1,
-                    "id": 1,
-                    "title": "accusamus beatae ad facilis cum similique qui sunt",
-                    "url": "https://via.placeholder.com/600/92c952",
-                    "thumbnailUrl": "https://via.placeholder.com/150/92c952"
-                },
-                {
-                    "albumId": 1,
-                    "id": 2,
-                    "title": "reprehenderit est deserunt velit ipsam",
-                    "url": "https://via.placeholder.com/600/771796",
-                    "thumbnailUrl": "https://via.placeholder.com/150/771796"
-                },
-                {
-                    "albumId": 1,
-                    "id": 3,
-                    "title": "officia porro iure quia iusto qui ipsa ut modi",
-                    "url": "https://via.placeholder.com/600/24f355",
-                    "thumbnailUrl": "https://via.placeholder.com/150/24f355"
-                },
-                {
-                    "albumId": 1,
-                    "id": 4,
-                    "title": "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
-                    "url": "https://via.placeholder.com/600/d32776",
-                    "thumbnailUrl": "https://via.placeholder.com/150/d32776"
-                },
-                {
-                    "albumId": 1,
-                    "id": 5,
-                    "title": "natus nisi omnis corporis facere molestiae rerum in",
-                    "url": "https://via.placeholder.com/600/f66b97",
-                    "thumbnailUrl": "https://via.placeholder.com/150/f66b97"
-                }
-            ];
-
-            $scope.getPictures = function () {
-                $http.get('https://jsonplaceholder.typicode.com/photos')
-                    .success(function (data) {
-                        $scope.pictures = data.slice(0,50);
-                    })
-            };
-
-            $scope.getPicturesOfAlbum = function (albumid) {
-                $http.get('https://jsonplaceholder.typicode.com/photos')
-                    .success(function (wynik) {
-                        // $scope.pictures = [];
-                        for(let pic of wynik) {
-                            if (pic.albumId===albumid) {
-                                $scope.pictures.push(pic);
-                            }
-                        }
-                    })
-            };
-
 
 
             // Deklaracja tablicy uczestników z początkowymi danymi
             $scope.uczestnicy = [
                 {
-                    'imie' : 'Tadeusz',
-                    'nazwisko' : 'Zakorczmenny',
-                    'wynik' : 3.5
-                }, {
-                    'imie' : 'Jacek',
-                    'nazwisko' : 'Zowakowski',
-                    'wynik' : 2.0
-                }, {
-                    'imie' : 'Sylwia',
-                    'nazwisko' : 'Polak',
+                    'imie' : '12',
+                    'nazwisko' : 'Pizza',
                     'wynik' : 3.0
                 }, {
-                    'imie' : 'Marzena',
-                    'nazwisko' : 'Polak',
-                    'wynik' : 3.5
+                    'imie' : '26',
+                    'nazwisko' : 'Kebab',
+                    'wynik' : 2
+                }, {
+                    'imie' : '73',
+                    'nazwisko' : 'Pomidorowa',
+                    'wynik' : 2
+                }, {
+                    'imie' : '24',
+                    'nazwisko' : 'Pierogi',
+                    'wynik' : 1
                 }
             ];
 
